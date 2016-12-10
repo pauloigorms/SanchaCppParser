81464fdc278c1894c7e537c35d03b05b501b88b6
pauloigormoraes <pauloigormoraes@gmail.com>
2016-12-09 22:19:45 -0400

    Criação de script para compilação dos arquivos do AL

---
+++ cpp/compile.sh
@@ -0,0 +1,12 @@
+#--
+# author @ Paulo Moraes [http://pauloigormoraes.com]
+# Data: 12/09/2016
+#--
+
+#--- Commands --- #
+
+javacc cpp.jj
+chmod 777 output/
+cd output/
+javac lexicalAnalyzer.java
+java lexicalAnalyzer
